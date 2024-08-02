d926847e6f0027113d35ae5235a334a0c92f1cc8
Anil <anil6996f@gmail.com>
2024-08-02 13:22:48 +0530

    completed user update functionality

--- api/index.js
+++ api/index.js
@@ -4,12 +4,18 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// app.use(cors({
+//     origin: 'http://localhost:5173', // Allow requests from your frontend
+//     credentials: true, // Allow credentials (like cookies) to be sent
+// }));
 
 
 mongoose.connect(process.env.MONGO).then(() => {
