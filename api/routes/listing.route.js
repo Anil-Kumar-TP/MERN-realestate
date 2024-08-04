705b65ab62e960f86d36e30613f1b4c9fe0465a9
Anil <anil6996f@gmail.com>
2024-08-04 22:14:11 +0530

    completed delete listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.controller.js';
+import { createListing,deleteListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing); // verified users can create listings
-
+router.delete('/delete/:id', verifyToken, deleteListing); // this id is the id of that particular listing.
 
 export default router;
