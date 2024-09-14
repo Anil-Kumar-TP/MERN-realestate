fa93cc943903534cbd5ff0c4056f9a2a92b24441
Anil <anil6996f@gmail.com>
2024-09-14 13:41:30 +0530

    added necessary code for deployment

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -25,11 +25,16 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log('cannot connect to DB');
 });
 
-
+const __dirname = path.resolve();
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
